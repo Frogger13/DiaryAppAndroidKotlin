7791395452532a4870f8819bbcdbcbcf62b2ca7a
Frogger13 <gfg071298@yandex.ru>
2021-02-27 16:36:15 +0300

    Deleted comments

--- app/src/main/java/com/androidapp/diaryapp/MainActivity.kt
+++ app/src/main/java/com/androidapp/diaryapp/MainActivity.kt
@@ -21,7 +21,10 @@ class MainActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        init()
+    }
 
+    fun init(){
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
